99a661c849176fba688d352726a35ffe31ec3d7f
ThatMG393 <thatmg393@gmail.com>
2022-01-11 19:17:29 +0800

    Initial commit

---
+++ discord-rpc-android/src/main/java/com/thatmg393/discordrpc/DiscordRPC.java
@@ -0,0 +1,13 @@
+package com.thatmg393.discordrpc;
+
+import android.content.Context;
+
+public class DiscordRPC
+{
+    Context context;
+
+    public DiscordRPC(Context context)
+    {
+        this.context = context;
+    }
+}
